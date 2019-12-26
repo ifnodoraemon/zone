25247e18176f2f00dcbf850b6aeffc1ec37ffc5a
ifnodoraemon <ifnodoraemon@gmail.com>
2019-12-26 18:28:13 +0800

    init

---
+++ src/test/java/com/ifnodoraemon/com/zone/ZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifnodoraemon.com.zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
